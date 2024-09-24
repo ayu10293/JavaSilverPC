ef6c9ea5efb3a294264d6f3017a8c12f7a03f567
ayu10293 <snof11513@gmail.com>
2024-09-24 11:38:23 +0900

    eclipse

--- JavaSilver/src/Q2_5.java
+++ JavaSilver/src/Q2_5.java
@@ -3,10 +3,10 @@ public class Q2_5 {
 
 	public static void main(String[] args) {
 		String[] array =new String[3];
-		array[1] = "10";
-		array[2] = "20";
-		array[3] = "30";
-		System.out.println(array[1] + array[2] + array[3]);
+		array[0] = "10";
+		array[1] = "20";
+		array[2] = "30";
+		System.out.println(array[0] + array[1] + array[2]);
 	}
 
 }
